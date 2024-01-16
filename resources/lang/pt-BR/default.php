f27bcbc0652c306c5ec9b3b06d2534af808cf887
Leandro <l3s.leandro@gmaial.com>
2024-01-15 23:06:38 -0300

    criando tradução

---
+++ resources/lang/pt-BR/default.php
@@ -0,0 +1,15 @@
+<?php
+ 
+return [
+    'title' => 'Futebol',
+    'players' => 'Jogadores',
+    'playersadd' => 'Novo Jogador',
+    'playersedit' => 'Editar Jogador',
+    'add' => 'Novo',
+    'name' => 'Nome',
+    'phone' => 'Telefone',
+    'leval' => 'Nivél',
+    'goalkeeper' => 'Goleiro',
+    'confirmed' => 'Confirmado',
+    'save' => 'Salvar'
+];
